b3d4306363ed02ff57135cd4eaa0a9b3b0a34dd2
Sumit-Kumar-1999 <sk3104647@gmail.com>
2023-09-18 20:46:14 +0530

    second commit

--- src/main/java/com/jshop/jshopspringbootproject/controller/ProductOrderController.java
+++ src/main/java/com/jshop/jshopspringbootproject/controller/ProductOrderController.java
@@ -1,6 +1,26 @@
 package com.jshop.jshopspringbootproject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jshop.jshopspringbootproject.dto.ProductOrder;
+import com.jshop.jshopspringbootproject.responsestructure.ResponseStructure;
+import com.jshop.jshopspringbootproject.service.ProductOrderService;
+
+
+@RestController
+@RequestMapping("/order")
 public class ProductOrderController {
 
+	@Autowired
+	private ProductOrderService orderService;
 	
+	@PutMapping("/productOrer/{productId}/{quantity}/{address}")
+	public ResponseStructure<ProductOrder> saveProductOrderController(@PathVariable int productId, @PathVariable int quantity,@PathVariable String address)
+	{
+		return orderService.saveProductOrderService(productId, quantity, address);
+	}
 }
--- src/main/java/com/jshop/jshopspringbootproject/service/ProductOrderService.java
+++ src/main/java/com/jshop/jshopspringbootproject/service/ProductOrderService.java
@@ -1,16 +1,71 @@
 package com.jshop.jshopspringbootproject.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
+import com.jshop.jshopspringbootproject.dao.ProductDao;
+import com.jshop.jshopspringbootproject.dao.ProductOrderDao;
+import com.jshop.jshopspringbootproject.dto.Product;
 import com.jshop.jshopspringbootproject.dto.ProductOrder;
+import com.jshop.jshopspringbootproject.responsestructure.ResponseStructure;
 
-public class ProductOrderService {
+import jakarta.servlet.http.HttpSession;
 
+@Service
+public class ProductOrderService 
+{
 	@Autowired
-	private ProductOrderService orderService;
+	private ProductDao productDao;
 	
-	public ProductOrder saveProductOrder(int productId, int quantity, String address)
+	@Autowired
+	private HttpSession httpSession;
+	
+	@Autowired
+	private ProductOrderDao orderDao;
+	
+	@Autowired
+	private ResponseStructure<ProductOrder> responseStructure;
+	
+	public ResponseStructure<ProductOrder> saveProductOrderService(int productId, int quantity, String address)
 	{
+		Product product = productDao.getProductByIdDao(productId);
 		
+		if((product!=null)&&(httpSession.getAttribute("password")!=null))
+		{
+			if(product.getProductVerified().equalsIgnoreCase("yes"))
+			{
+				if(quantity<=product.getProductQuantity())
+				{
+					ProductOrder productOrder = orderDao.saveProductOrder(productId, quantity, address);
+					responseStructure.setStatusCode(HttpStatus.ACCEPTED.value());
+					responseStructure.setStatusMsg("Product order success");
+					responseStructure.setDescription("Your order will delivere in 3 days");
+					responseStructure.setData(productOrder);
+				}
+				else
+				{
+					responseStructure.setStatusCode(HttpStatus.NOT_ACCEPTABLE.value());
+					responseStructure.setStatusMsg("Product is not available");
+					responseStructure.setDescription("Stack khtam ho gya");
+					responseStructure.setData(null);
+				}
+			}
+			else
+			{
+				responseStructure.setStatusCode(HttpStatus.NOT_ACCEPTABLE.value());
+				responseStructure.setStatusMsg("Product is not verified");
+				responseStructure.setDescription("Wait for the verification to purchase");
+				responseStructure.setData(null);
+			}
+		}
+		else
+		{
+			responseStructure.setStatusCode(HttpStatus.NOT_ACCEPTABLE.value());
+			responseStructure.setStatusMsg("Please! login first with user");
+			responseStructure.setDescription("Kripya pahle login kare");
+			responseStructure.setData(null);
+		}
+		return responseStructure;
 	}
 }
